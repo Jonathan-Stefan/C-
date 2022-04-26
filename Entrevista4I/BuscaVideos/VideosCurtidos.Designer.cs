f1a0aea09f4c7945460196cd04fe047739779257
jonathan <jonathan.stefan@inatel.br>
2022-04-26 00:29:05 -0300

    Adiciona videos em uma lista e mostra os marcados

---
+++ Entrevista4I/BuscaVideos/VideosCurtidos.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BuscaVideos
+{
+    partial class videosCurtidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listaVideos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listaVideos
+            // 
+            this.listaVideos.Location = new System.Drawing.Point(12, 12);
+            this.listaVideos.Multiline = true;
+            this.listaVideos.Name = "listaVideos";
+            this.listaVideos.ReadOnly = true;
+            this.listaVideos.Size = new System.Drawing.Size(386, 504);
+            this.listaVideos.TabIndex = 0;
+            // 
+            // videosCurtidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 528);
+            this.Controls.Add(this.listaVideos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "videosCurtidos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "videosCurtidos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox listaVideos;
+    }
+}
---
+++ Entrevista4I/BuscaVideos/listaDeCurtidos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaVideos
+{
+    public partial class listaDeCurtidos : Form
+    {
+        public listaDeCurtidos()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_mostrarselecionados_Click(object sender, EventArgs e)
+        {
+            String txt = "";
+            foreach(string t in listaGostei.CheckedItems)
+            {
+                txt += t + ", ";
+            }
+    
+
+            MessageBox.Show(txt);
+        }
+
+        private void bt_adicionarNovoVideo_Click(object sender, EventArgs e)
+        {
+            if (tb_addVideo.Text != "")
+            {
+                listaGostei.Items.Add(tb_addVideo.Text);
+                tb_addVideo.Clear(); ;
+                tb_addVideo.Focus();
+            }
+        }
+    }
+}
